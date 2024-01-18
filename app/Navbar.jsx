2252bc906e2a63f438146af123c15dddaffb08fa
padermo <fabioaeh96@gmail.com>
2024-01-18 14:38:26 -0500

    agregando seccion time

--- app/Navbar.jsx
+++ app/Navbar.jsx
@@ -8,6 +8,7 @@ function Navbar() {
         <Link href={'/'} className='navbar__link'>Slide 3D</Link>
         <Link href={'/slide-normal'} className='navbar__link'>Slide Normal</Link>
         <Link href={'/slide-coverflow'} className='navbar__link'>Slide Coverflow</Link>
+        <Link href={'/time'} className='navbar__link'>Time</Link>
       </div>
     </nav>
   )
---
+++ app/time/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './time.css'
+
+function page() {
+  return (
+    <div className='time_content'>
+      <input type="date" placeholder='fecha' className='time_input' />
+    </div>
+  )
+}
+
+export default page
