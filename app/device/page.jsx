5428004da4dc038a4e80098209dd132df1f0f9c5
padermo <fabioaeh96@gmail.com>
2024-01-23 10:06:48 -0500

    subiendo modificacion del agent

--- app/device/page.jsx
+++ app/device/page.jsx
@@ -5,7 +5,24 @@ function Device() {
   const [device, setDevice] = useState('')
   
   useEffect(()=>{
-    setDevice(window.navigator.userAgent)
+    const userAgent = window.navigator.userAgent;
+    let os = '';
+
+    if (/Windows/.test(userAgent)) {
+      os = 'Windows';
+    } else if (/Macintosh/.test(userAgent)) {
+      os = 'Macintosh';
+    } else if (/Linux/.test(userAgent)) {
+      os = 'Linux';
+    } else if (/Android/.test(userAgent)) {
+      os = 'Android';
+    } else if (/iPhone|iPad|iPod/.test(userAgent)) {
+      os = 'iOS';
+    } else {
+      os = 'Unknown';
+    }
+
+    setDevice(os)
   },[])
 
   return (
