cb23336bbd5ecf2e88a1636f239af63068b88b80
padermo <fabioaeh96@gmail.com>
2024-01-23 10:30:40 -0500

    removiendo agent

--- app/device/page.jsx
+++ app/device/page.jsx
@@ -1,33 +1,31 @@
 'use client'
 import React, {useEffect, useState} from 'react'
+import {isAndroid, isIOS, isWindows, isMacOs, deviceType, osName, osVersion, browserName} from 'react-device-detect'
 
 function Device() {
-  const [device, setDevice] = useState('')
+  const [device, setDevice] = useState({
+    os: osName,
+    os_version: osVersion,
+    type: deviceType,
+    name:browserName,
+    date:new Date()
+  })
   
-  useEffect(()=>{
-    const userAgent = window.navigator.userAgent;
-    let os = '';
-
-    if (/Android/.test(userAgent)) {
-      os = 'Android';
-    } else if (/iPhone|iPad|iPod/.test(userAgent)) {
-      os = 'iOS';
-    } else if (/Windows/.test(userAgent)) {
-      os = 'Windows';
-    } else if (/Macintosh/.test(userAgent)) {
-      os = 'Macintosh';
-    } else if (/Linux/.test(userAgent)) {
-      os = 'Linux';
-    } else {
-      os = 'Unknown';
-    }
-
-    setDevice(os)
-  },[])
+  // useEffect(()=>{
+    
+  //   console.log('isAndroid', isAndroid);
+  //   console.log('isIOS', isIOS);
+  //   console.log('isWindows', isWindows);
+  //   console.log('isMacOs', isMacOs);
+  //   console.log('deviceType', deviceType);
+  //   console.log('osName', osName);
+  //   console.log('osVersion', osVersion);
+  //   console.log('browserName', browserName);
+  // },[])
 
   return (
     <div>
-      {device && device}
+      {JSON.stringify(device, null, '\n')}
     </div>
   )
 }
